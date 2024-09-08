483d15c9a95ae5aeb68138b440394064da1a7e12
DAKExDUCK <arsengabdulin228@gmail.com>
2024-09-08 23:03:47 +0500

    [fix] check lint by pylint

--- modules/utils/config/__init__.py
+++ modules/utils/config/__init__.py
@@ -2,21 +2,20 @@ import os
 from typing import Any
 
 from . import exceptions
-from .exceptions import ConfigFieldIsRequired, ConfigFieldWrongType
 
 
 def get_from_env(field: str, default: Any | None = None, value_type: type[str | int | Any] = str) -> int | str | Any:
     value = os.getenv(field, default)
     if value is None and default is None:
-        raise ConfigFieldIsRequired(field)
+        raise exceptions.ConfigFieldIsRequired(field)
 
     if not isinstance(value, value_type):
         if isinstance(value, str) and value_type is int:
             try:
                 return int(value)
             except Exception as exc:
-                raise ConfigFieldWrongType(field, value, value_type) from exc
-        raise ConfigFieldWrongType(field, value, value_type)
+                raise exceptions.ConfigFieldWrongType(field, value, value_type) from exc
+        raise exceptions.ConfigFieldWrongType(field, value, value_type)
 
     return value
 
