daea122656dff14fe2132b328e4b554e074b38a8
amdRyzen5600X <arca26100@gmail.com>
2024-09-09 17:23:28 +0500

    FEAT: endpoint for notification status

---
+++ routers/notification.py
@@ -0,0 +1,36 @@
+from typing import Any
+
+from fastapi import APIRouter, HTTPException, status
+
+from modules.database.models import NotificationStatus
+from modules.database.notification import NotificationDB
+
+router = APIRouter(
+    prefix="/notification",
+    tags=["notification"],
+    # dependencies=[Depends(get_token_header)],
+    responses={404: {"desc": "Not found"}},
+)
+
+
+@router.get("/{user_id}")
+async def get_notification_status(user_id: int) -> NotificationStatus:
+    notification_status = await NotificationDB.get_notification_status(user_id=user_id)
+    if not notification_status:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"no notification statuses found for user with {user_id}"
+        )
+
+    return notification_status
+
+
+@router.post("/{user_id}")
+async def set_notification_status(user_id: int, key: str, state: bool) -> dict[str, Any]:
+    try:
+        await NotificationDB.set_notification_status(user_id=user_id, key=key, state=state)
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT, detail=f"unnable to set notification status for user {user_id}\n{e}"
+        ) from e
+
+    return {"success": True, "desc": f"notification status {key} for user {user_id=} is set to {state}"}
